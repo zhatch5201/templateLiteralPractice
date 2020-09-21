feebeb47b644d0796a94f3fc2f46e6531d7b133e
Jordan-Gillispie <60756061+Jordan-Gillispie@users.noreply.github.com>
2020-09-21 11:08:58 -0700

    template literal practice

---
+++ temLit.js
@@ -0,0 +1,31 @@
+// Name:
+// Date:
+// Assignment: Template Literal Practice
+
+////////////////////////Pt. 1////////////////////////
+// create a var that accepts a first name as a prompt
+
+
+// create a var that accepts a last name as a prompt
+
+
+// Create a variable that will hold a score and a var that will hold a full name 
+
+
+// create a var that adds the full name var and score in a message
+
+
+////////////////////////Pt. 2////////////////////////
+//comment out the lines above then create two var's that hold numbers
+
+
+//create an add var that adds both vars
+
+
+//console log the newly created add var
+
+
+//console log 3 string's using concatination that say "concatinating is easy""when using template literals,""I can even embed numbers and not have to worry about spacing!"
+
+
+//console log the three strings above and add a third string that includes the add var
